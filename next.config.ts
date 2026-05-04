e41d105c521eb295b834feb51422b35dd2996b7b
sadSanta-07 <iffcogc34@gmail.com>
2026-05-04 23:06:19 +0530

    fix

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      "lh3.googleusercontent.com",
-      "i.pravatar.cc",            
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+      },
     ],
   },
 };
